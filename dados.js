218cd4ca49c60d435c4f9f596513955a6f93102d
JoaoPedroSSLima <joaopedrosl2003@gmail.com>
2024-09-09 22:54:13 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -17,6 +17,51 @@ let jogos = [
         descricao: "Gears of War: Uma franquia de jogos de tiro em terceira pessoa, ambientada em um planeta devastado por guerra, onde os humanos lutam contra uma raça de criaturas subterrâneas conhecidas como Locust.",
         link: "https://pt.wikipedia.org/wiki/Gears_of_war",
         tags:"guerra apocalipse tiro" 
-    }
+  
+  
+  
+    },
+{
+    titulo: "Sunset Overdrive",
+    descricao: "Sunset Overdrive: Um jogo de ação e aventura em mundo aberto, ambientado em uma cidade pós-apocalíptica infestada por mutantes, onde o protagonista utiliza parkour e armas inusitadas para sobreviver.",
+    link: "https://pt.wikipedia.org/wiki/Sunset_Overdrive",
+    tags:"ação aventura mundo aberto"
+},
+{
+    titulo: "Ryse: Son of Rome",
+    descricao: "Ryse: Son of Rome: Um jogo de ação em terceira pessoa, ambientado na Roma Antiga, com foco em combates brutais e visuais impressionantes.",
+    link: "https://pt.wikipedia.org/wiki/Ryse:_Son_of_Rome",
+    tags:"ação história roma antiga"
+},
+{
+    titulo: "Hellblade: Senua's Sacrifice",
+    descricao: "Hellblade: Senua's Sacrifice: Um jogo de aventura psicológico, ambientado na mitologia nórdica, com foco na história de Senua, uma guerreira picta com psicose, em sua jornada para salvar a alma de seu amado.",
+    link: "https://pt.wikipedia.org/wiki/Hellblade:_Senua's_Sacrifice",
+    tags:"aventura psicológico mitologia"
+},
+{
+    titulo: "The Witcher 3: Wild Hunt",
+    descricao: "The Witcher 3: Wild Hunt: Um RPG de mundo aberto, ambientado em um universo de fantasia medieval, onde você controla Geralt de Rivia, um caçador de monstros.",
+    link: "https://pt.wikipedia.org/wiki/The_Witcher_3:_Wild_Hunt",
+    tags:"rpg mundo aberto fantasia"
+},
+{
+    titulo: "Red Dead Redemption 2",
+    descricao: "Red Dead Redemption 2: Um jogo de mundo aberto ambientado no Velho Oeste, com foco em história, personagens e mecânicas de sobrevivência.",
+    link: "https://pt.wikipedia.org/wiki/Red_Dead_Redemption_2",
+    tags:"mundo aberto faroeste história"
+},
+{
+    titulo: "The Legend of Zelda: Breath of the Wild",
+    descricao: "The Legend of Zelda: Breath of the Wild: Um jogo de ação e aventura em mundo aberto, onde você explora o reino de Hyrule com liberdade total e resolve puzzles desafiadores.",
+    link: "https://pt.wikipedia.org/wiki/The_Legend_of_Zelda:_Breath_of_the_Wild",
+    tags:"aventura mundo aberto zelda"
+},
+{
+    titulo: "Dark Souls",
+    descricao: "Dark Souls: Uma série de RPGs de ação com dificuldade elevada, ambientada em um mundo sombrio e gótico, onde você enfrenta desafios brutais e criaturas perigosas.",
+    link: "https://pt.wikipedia.org/wiki/Dark_Souls",
+    tags:"rpg ação desafio"
+}
+    
 ];
-
